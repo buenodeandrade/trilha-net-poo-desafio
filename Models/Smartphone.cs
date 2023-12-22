09e9dd47cfe603b0d5e812020c2458f579b7964b
buenodeandrade <felipebuenodeandrade@gmail.com>
2023-12-22 15:51:30 -0300

    Feitas as alterações solicitadas e implantadas restrições de composição de número e imei.

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,22 @@
+using System.Text.RegularExpressions;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        // Implementa as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        public int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
-            Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            // Passa os parâmetros do construtor para as propriedades
+            Numero = ValidarNumero(numero);
+            Modelo = modelo;
+            IMEI = ValidarIMEI(imei);
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -21,6 +29,29 @@ namespace DesafioPOO.Models
             Console.WriteLine("Recebendo ligação...");
         }
 
-        public abstract void InstalarAplicativo(string nomeApp);
+        public virtual void InstalarAplicativo(string nomeApp)
+         {
+            Console.WriteLine($"App {nomeApp} instalado com sucesso");
+        }
+
+  private string ValidarNumero(string numero)
+        {
+            string pattern = @"^[0-9]+$";
+            if (!Regex.IsMatch(numero, pattern))
+            {
+                throw new ArgumentException("Numero must only contain numbers");
+            }
+            return numero;
+        }
+
+        private string ValidarIMEI(string imei)
+        {
+            string pattern = @"^[0-9]{15,17}$";
+            if (!Regex.IsMatch(imei, pattern))
+            {
+                throw new ArgumentException("IMEI deve ter de 15 a 17 caracteres e conter apenas números.");
+            }
+            return imei;
+        }
     }
 }
